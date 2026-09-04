aec84d451bbfb08d4f8d56405820416288a3802f
Eyder Rios <eyder.rios@gmail.com>
2026-09-04 10:59:27 -0300

    Aula 2026-09-04 - DiceApp

--- dices/lib/dice_panel.dart
+++ dices/lib/dice_panel.dart
@@ -12,23 +12,51 @@ class DicePanel extends StatefulWidget {
 }
 
 class _DicePanelState extends State<DicePanel> {
-  final Random rnd = Random();
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 4;
+  final rnd = Random();
+
+  int leftDiceNumber = 0;
+  int rightDiceNumber = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    leftDiceNumber = _rollDice();
+    rightDiceNumber = _rollDice();
+  }
+
+  int _rollDice() {
+    return rnd.nextInt(6) + 1;
+  }
+
+  void _rollBothDices() {
+    _rollLeftDice();
+    _rollRightDice();
+  }
+
+  void _rollLeftDice() {
+    setState(() {
+      leftDiceNumber = _rollDice();
+    });
+  }
+
+  void _rollRightDice() {
+    setState(() {
+      rightDiceNumber = _rollDice();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          leftDiceNumber = rnd.nextInt(6) + 1;
-          rightDiceNumber = rnd.nextInt(6) + 1;
-        });
-        debugPrint('TAP!!!! [l=$leftDiceNumber, r=$rightDiceNumber]');
-      },
+      onTap: _rollBothDices,
+      behavior: HitTestBehavior.opaque,
       child: Center(
         child: Row(
-          children: [DiceWidget(leftDiceNumber), DiceWidget(rightDiceNumber)],
+          children: [
+            DiceWidget(diceNumber: leftDiceNumber, onTap: _rollLeftDice),
+            DiceWidget(diceNumber: rightDiceNumber, onTap: _rollRightDice),
+          ],
         ),
       ),
     );
--- dices/lib/dice_widget.dart
+++ dices/lib/dice_widget.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 
 class DiceWidget extends StatelessWidget {
   final int diceNumber;
+  final VoidCallback? onTap;
 
-  const DiceWidget(this.diceNumber, {super.key});
+  const DiceWidget({super.key, required this.diceNumber, this.onTap});
 
   @override
   Widget build(Object context) {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Image.asset('assets/images/dice$diceNumber.png'),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.asset('assets/images/dice$diceNumber.png'),
+        ),
       ),
     );
   }
